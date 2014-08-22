7411d6e912f5d36b07c60c5a1d8cc11c256cc8ac
birkin <birkin.diana@gmail.com>
2014-08-22 10:32:38 -0400

    updates tests (all pass).

--- tests.py
+++ tests.py
@@ -5,7 +5,7 @@ import usep_app
 from django.test import TestCase
 from usep_app import settings_app
 # from usep_app.models import Repository
-from usep_app.models import Collection, Inscription2, Publication, Publications
+from usep_app.models import Collection, Publication, Publications
 
 log = logging.getLogger(__name__)
 
@@ -24,7 +24,7 @@ class CollectionTest( TestCase ):
     self.assertEqual( list, type(inscription_list) )
     self.assertEqual( dict, type(inscription_list[0]) )
     list_element_keys = sorted( inscription_list[0].keys() )
-    self.assertEqual( [ u'id', u'status' ], list_element_keys )
+    self.assertEqual( [u'graphic_name', u'id', u'status'], list_element_keys )
 
   def test_enhance_solr_data(self):
     c = Collection()
@@ -33,41 +33,41 @@ class CollectionTest( TestCase ):
     self.assertEqual( list, type(enhanced_list) )
     self.assertEqual( dict, type(enhanced_list[0]) )
     list_element_keys = sorted( enhanced_list[0].keys() )
-    self.assertEqual( [u'id', u'image_url', u'status', u'url'], list_element_keys )
+    self.assertEqual( [u'graphic_name', u'id', u'image_url', u'status', u'url'], list_element_keys )
 
   # end class CollectionTest()
 
 
-class Inscription2Test(TestCase):
+# class Inscription2Test(TestCase):
 
-  def test_extract_inscription_data_a(self):  # more info
-    inscription_id = u'CA.Berk.UC.HMA.G.8-3898'
-    insc = Inscription2()
-    insc.run_xslt( inscription_id )
-    insc.update_xslt_html()
-    insc.extract_inscription_data()
-    self.assertEqual( u'Fragment', insc.extracted_data[u'attributes'][u'Condition'] )
-    self.assertEqual( [u'Smutny, R.J., Greek and Latin Inscriptions at Berkeley (1966): 6-7, no. 3, Pl. 5'], insc.extracted_data[u'bibl'] )
+#   def test_extract_inscription_data_a(self):  # more info
+#     inscription_id = u'CA.Berk.UC.HMA.G.8-3898'
+#     insc = Inscription2()
+#     insc.run_xslt( inscription_id )
+#     insc.update_xslt_html()
+#     insc.extract_inscription_data()
+#     self.assertEqual( u'Fragment', insc.extracted_data[u'attributes'][u'Condition'] )
+#     self.assertEqual( [u'Smutny, R.J., Greek and Latin Inscriptions at Berkeley (1966): 6-7, no. 3, Pl. 5'], insc.extracted_data[u'bibl'] )
 
-  def test_extract_inscription_data_b(self):  # less info
-    inscription_id = u'CA.Berk.UC.HMA.G.8-4985'
-    insc = Inscription2()
-    insc.run_xslt( inscription_id )
-    insc.update_xslt_html()
-    insc.extract_inscription_data()
-    self.assertEqual( u'', insc.extracted_data[u'attributes'][u'Condition'] )
-    self.assertEqual( [u'Classical Attic Tombstones 3: 415', u'SEG 35 (1985): no. 202'], insc.extracted_data[u'bibl'] )
+#   def test_extract_inscription_data_b(self):  # less info
+#     inscription_id = u'CA.Berk.UC.HMA.G.8-4985'
+#     insc = Inscription2()
+#     insc.run_xslt( inscription_id )
+#     insc.update_xslt_html()
+#     insc.extract_inscription_data()
+#     self.assertEqual( u'', insc.extracted_data[u'attributes'][u'Condition'] )
+#     self.assertEqual( [u'Classical Attic Tombstones 3: 415', u'SEG 35 (1985): no. 202'], insc.extracted_data[u'bibl'] )
 
-  def test_extract_inscription_data_bib_only(self):
-    inscription_id = u'IL.Urb.UI.WHM.G.WHM1930.01.0001'
-    insc = Inscription2()
-    insc.run_xslt( inscription_id )
-    insc.update_xslt_html()
-    insc.extract_inscription_data()
-    self.assertEqual( {}, insc.extracted_data[u'attributes'] )
-    self.assertEqual( [u'BE (1973): no. 24', u'Kroll, J. H., Athenian Bronze Allotment Plates (1972): 124-126, no. 18'], insc.extracted_data[u'bibl'] )
+#   def test_extract_inscription_data_bib_only(self):
+#     inscription_id = u'IL.Urb.UI.WHM.G.WHM1930.01.0001'
+#     insc = Inscription2()
+#     insc.run_xslt( inscription_id )
+#     insc.update_xslt_html()
+#     insc.extract_inscription_data()
+#     self.assertEqual( {}, insc.extracted_data[u'attributes'] )
+#     self.assertEqual( [u'BE (1973): no. 24', u'Kroll, J. H., Athenian Bronze Allotment Plates (1972): 124-126, no. 18'], insc.extracted_data[u'bibl'] )
 
-  # end class Inscription2Test()
+#   # end class Inscription2Test()
 
 
 class PublicationTest( TestCase ):
@@ -77,7 +77,7 @@ class PublicationTest( TestCase ):
     id_list = [ u'KY.Lou.SAM.L.1929.17.753', u'MI.AA.UM.KM.L.1038' ]
     pub.getPubData( id_list )
     # pprint.pprint( pub.inscription_entries )
-    self.assertEqual( sorted(pub.inscription_entries[0].keys()), [u'id', u'status'] )
+    self.assertEqual( [u'graphic_name', u'id', u'status'], sorted(pub.inscription_entries[0].keys()) )
 
   # end class PublicationTest()
 
@@ -172,6 +172,6 @@ class PublicationsTest( TestCase ):
     # self.assertEqual( pubs.monographs_dict[u'Ancient Art from the V. G. Simkhovitch Collection'][0], u'IN.Bloom.IUAM.G.63.105.34' )
     # print u'pubs.monographs_dict...'; pprint.pprint( pubs.monographs_dict )
     # print u'pubs.monographs...'; pprint.pprint( pubs.monographs )
-    self.assertEqual( len(pubs.master_pub_dict.keys()), 177 )
+    self.assertTrue( len(pubs.master_pub_dict.keys()) > 150 )
 
   # end class PublicationsTest()
