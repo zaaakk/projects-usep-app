c29d3a09de4a7b18af458e05d211ebe666bd35e0
birkin <birkin.diana@gmail.com>
2016-05-04 11:48:49 -0400

    adds model to admin.

--- browse_app/admin.py
+++ browse_app/admin.py
@@ -1,4 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
+
+from .models import FlatCollection
 from django.contrib import admin
+
+
+class FlatCollectionAdmin( admin.ModelAdmin ):
+    # date_hierarchy = 'sent'
+    # list_display = [ 'sent', 'sender', 'email' ]
+    ordering = [ '-id' ]
+
+
+admin.site.register( FlatCollection, FlatCollectionAdmin )
