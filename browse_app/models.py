2948520c65d8476ebb12ea19aaab55a4ff4507b7
birkin <birkin.diana@gmail.com>
2016-05-04 11:59:07 -0400

    adds required import.

--- browse_app/models.py
+++ browse_app/models.py
@@ -6,6 +6,7 @@ from django.conf import settings as project_settings
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.http import HttpResponseRedirect
+from django.utils.encoding import smart_unicode
 
 
 log = logging.getLogger(__name__)
