ecb88248c0be4f1d8e1c59505861875a751ae2bf
birkin <birkin.diana@gmail.com>
2016-05-10 10:39:12 -0400

    hooks up search.

--- browse_app/urls_app.py
+++ browse_app/urls_app.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
+
 from django.conf.urls import patterns, include, url
 from django.views.generic import RedirectView
 
@@ -10,6 +11,8 @@ urlpatterns = patterns('',
     url( r'^info/$',  'browse_app.views.hi', name='info_url' ),
 
     url( r'^collections/$',  'browse_app.views.collections', name='collections_url' ),
+
+    url( r'^search/$', 'browse_app.search.search_form', name='search_url'),
 
     url( r'^$',  RedirectView.as_view(pattern_name='info_url') ),
 
