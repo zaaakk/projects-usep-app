7d230489f67d69aca3767a7be79ab9074964bf41
birkin <birkin.diana@gmail.com>
2016-05-10 10:36:07 -0400

    adds setting.

--- browse_app/settings_app.py
+++ browse_app/settings_app.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
+
 import os
 
 
-FOO = 'BAR'
-
-# FOO2 = os.environ['FOO2']
-
 LOGIN_URL = unicode( os.environ['USEPWEB__LOGIN_URL'] )
+
+SOLR_URL_BASE = unicode( os.environ['USEPWEB__SOLR_URL_BASE'] )
