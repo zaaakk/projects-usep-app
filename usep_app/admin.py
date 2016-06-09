00e18580a08dcb5c9ad430b1a5e383bf5c369c30
birkin <birkin.diana@gmail.com>
2016-06-09 14:29:18 -0400

    shows models in admin.

--- usep_app/admin.py
+++ usep_app/admin.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 
 from .models import FlatCollection
 from django.contrib import admin
+from .models import AboutPage, ContactsPage, LinksPage, PublicationsPage, TextsPage
+
 
 
 class FlatCollectionAdmin( admin.ModelAdmin ):
@@ -13,3 +15,10 @@ class FlatCollectionAdmin( admin.ModelAdmin ):
 
 
 admin.site.register( FlatCollection, FlatCollectionAdmin )
+
+## static pages
+admin.site.register( AboutPage )
+admin.site.register( ContactsPage )
+admin.site.register( LinksPage )
+admin.site.register( PublicationsPage )
+admin.site.register( TextsPage )
